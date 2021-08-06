cc50117f97d416eed20487334957c07bd818428e
gksudershan <gksudershan@gmail.com>
2021-08-06 16:58:05 +0530

    Update to String program

--- Strings/src/com/mphasis/main/cui/Main.java
+++ Strings/src/com/mphasis/main/cui/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         String string1 = new String("First");
         String string2 = new String("First");
-        System.out.println(string1==string2);
+        System.out.println(string1==string2); //compares objects
+        System.out.println(string1.equals(string2)); //compares data
     }
 }
