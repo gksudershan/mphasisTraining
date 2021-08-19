f50a65dddc83e0759ac00cff3427f9d62ca96acc
gksudershan <gksudershan@gmail.com>
2021-08-19 18:10:38 +0530

    RMIApplication,JDBC

---
+++ JDBC/src/com/mphasis/main/cui/Main.java
@@ -0,0 +1,88 @@
+package com.mphasis.main.cui;
+
+import sun.dc.pr.PRError;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            //Register the Driver
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+
+            //Establish Connection
+           connection = DriverManager.getConnection("jdbc:oracle:thin:@DESKTOP-45GJQ8N:1521:orcl1","hr","hrpass");
+            System.out.println("Connected");
+
+            //Query the database
+            //preparedStatement = connection.prepareStatement("SELECT FIRST_NAME,LAST_NAME FROM EMPLOYEES WHERE DEPARTMENT_ID=? ");
+            /*preparedStatement = connection.prepareStatement("INSERT INTO USERS VALUES(?,?,?,?,?)");
+
+            preparedStatement.setInt(1,2);
+            preparedStatement.setString(2,"Allen");
+            preparedStatement.setString(3,"Jane");
+            preparedStatement.setString(4,"4567");
+            preparedStatement.setString(5,"A");*/
+
+            preparedStatement = connection.prepareStatement("UPDATE USERS SET STATUS=? WHERE ID=?");
+
+            preparedStatement.setString(1,"D");
+            preparedStatement.setInt(2,2);
+
+            int rowsEffected = preparedStatement.executeUpdate();
+            System.out.println(rowsEffected);
+            connection.commit();
+            //preparedStatement.setString(1,"30");
+
+           //resultSet =  preparedStatement.executeQuery();
+
+            if (resultSet != null) {
+                while(resultSet.next()){
+                    System.out.println(resultSet.getString(1)+" "+resultSet.getString(2));
+                }
+            }
+
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            try{
+                if(connection!=null && !connection.isClosed()){
+                    connection.rollback();
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+
+        }finally{
+            try {
+                if(resultSet!=null && !resultSet.isClosed()){
+                    resultSet.close();
+                }
+                if(preparedStatement!=null && !preparedStatement.isClosed()){
+                    preparedStatement.close();
+                }
+                if(connection!=null && !connection.isClosed()){
+                    connection.close();
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
